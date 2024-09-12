5d61b63c2ce63fd2212ce7f51ae33853f971a06b
bschopparapu <bschopparapu@neogov.net>
2024-09-12 15:39:56 -0700

    added java app

---
+++ java-app/src/main/java/com/yacoder/javalogdemo/JavaApp.java
@@ -0,0 +1,13 @@
+package com.yacoder.javalogdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(com.yacoder.javalogdemo.JavaApp.class, args);
+	}
+
+}
